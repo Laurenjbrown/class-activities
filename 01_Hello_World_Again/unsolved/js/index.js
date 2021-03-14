55137d79dbc703ec8393b1b124a6d72a6ebaf732
Lauren J Brown <45368488+Laurenjbrown@users.noreply.github.com>
2021-03-14 09:35:20 -0400

    Saturday 3.13
    Class and homework

---
+++ 01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,19 @@
+// Create a variable below
+
+
+var helloWorld = "Hello, is it me you're looking for"
+
+
+// Console.log() your variable here.
+console.log(helloWorld)
+
+alert(helloWorld)
+
+var kidsinhouse = "3";
+var kidsoutside = "2";
+var z = kidsoutside + kidsinhouse
+function addkids() {
+ alert (z)
+}
+
+addkids()
